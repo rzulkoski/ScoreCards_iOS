6678976700794c8490d47b50e4d8bcb45af43692
Ryan Zulkoski <ryan.zulkoski@gmail.com>
2012-08-26 10:07:56 -0500

    Initial Commit

---
+++ ScoreCards/ScoreCardsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ScoreCardsAppDelegate.h
+//  ScoreCards
+//
+//  Created by Ryan Zulkoski on 8/26/12.
+//  Copyright (c) 2012 RZGamer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScoreCardsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScoreCards/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ScoreCards
+//
+//  Created by Ryan Zulkoski on 8/26/12.
+//  Copyright (c) 2012 RZGamer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ScoreCardsAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ScoreCardsAppDelegate class]));
+    }
+}
